c6bb97bfcba465017062023dc5563ce9bd145142
aegis301 <57251443+aegis301@users.noreply.github.com>
2022-07-01 23:02:52 +0200

    rle mask functions

---
+++ utils/converter.py
@@ -0,0 +1,39 @@
+
+# The following functions are drawn from https://www.kaggle.com/paulorzp/rle-functions-run-length-encode-decode
+import numpy as np
+
+def mask2rle(img):
+    """
+    Takes in the image mask, returns string representation of the mask
+
+    Parameters
+    ----------
+    img : numpy array
+        Numpy array of the mask. 1 - mask, 0 - background
+
+    Returns
+    -------
+    string
+        string representation of the mask
+    """
+    pixels= img.T.flatten()
+    pixels = np.concatenate([[0], pixels, [0]])
+    runs = np.where(pixels[1:] != pixels[:-1])[0] + 1
+    runs[1::2] -= runs[::2]
+    return ' '.join(str(x) for x in runs)
+ 
+def rle2mask(mask_rle, shape=(1600,256)):
+    '''
+    mask_rle: run-length as string formated (start length)
+    shape: (width,height) of array to return 
+    Returns numpy array, 1 - mask, 0 - background
+
+    '''
+    s = mask_rle.split()
+    starts, lengths = [np.asarray(x, dtype=int) for x in (s[0:][::2], s[1:][::2])]
+    starts -= 1
+    ends = starts + lengths
+    img = np.zeros(shape[0]*shape[1], dtype=np.uint8)
+    for lo, hi in zip(starts, ends):
+        img[lo:hi] = 1
+    return img.reshape(shape).T
